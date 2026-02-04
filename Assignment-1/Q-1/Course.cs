48707b5a729468bc4cd62601a25b23b7a85fb201
Dhruvisha <dhruvibhaliya01@gmail.com>
2026-02-04 13:26:24 +0530

    Assignment - 1

---
+++ Assignment-1/Q-1/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Course : Content
+    {
+        public int Duration { get; set; }
+
+        public Course(int id, string title, int duration)
+            : base(id, title)
+        {
+            Duration = duration;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine($"{Id} - {Title} - {Duration} hrs");
+        }
+    }
+}
---
+++ Assignment-1/Q-1/LiveCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class LiveCourse : Course
+    {
+        public int LiveStudents { get; set; }
+
+        public LiveCourse(int id, string title, int duration, int students)
+            : base(id, title, duration)
+        {
+            LiveStudents = students;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine($"{Id} - {Title} - Live: {LiveStudents}");
+        }
+    }
+}
