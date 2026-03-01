ca3d6d37b2b14ed08a5128d778e8e4e7c28fef2e
Dhruvisha <dhruvibhaliya01@gmail.com>
2026-03-01 21:19:12 +0530

    Student Management API With React Demo

---
+++ StudentManagementAPI/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagementAPI.Models;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string? CourseName { get; set; }
+
+    public string? Duration { get; set; }
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+}
