338ed340935a933eb33f2eb4039f656e1b0e205c
Dhruvisha <dhruvibhaliya01@gmail.com>
2026-03-01 21:43:57 +0530

    React WIth .NET Core Demo

--- Assignment-2/Assignment-2_2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment_2_2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
